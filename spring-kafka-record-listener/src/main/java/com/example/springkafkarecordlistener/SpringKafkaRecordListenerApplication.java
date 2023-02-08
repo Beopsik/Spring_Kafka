362c53368d671d03350d4dd758d3e028c01afd16
Beopsik <mbsik6082@naver.com>
2023-02-08 17:03:34 +0900

    Add spring-kafka-record-listener

--- spring-kafka-record-listener/src/main/java/com/example/springkafkarecordlistener/SpringKafkaRecordListenerApplication.java
+++ spring-kafka-record-listener/src/main/java/com/example/springkafkarecordlistener/SpringKafkaRecordListenerApplication.java
@@ -1,13 +1,49 @@
 package com.example.springkafkarecordlistener;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.PartitionOffset;
+import org.springframework.kafka.annotation.TopicPartition;
 
 @SpringBootApplication
 public class SpringKafkaRecordListenerApplication {
+    public static Logger logger = LoggerFactory.getLogger(SpringKafkaRecordListenerApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringKafkaRecordListenerApplication.class, args);
+    }
+
+    @KafkaListener(topics = "test", groupId = "test-group-00")
+    public void recordListener(ConsumerRecord<String, String> record) {
+        logger.info(record.toString());
+    }
+
+    @KafkaListener(topics = "test", groupId = "test-group-01")
+    public void singleTopicListener(String messageValue) {
+        logger.info(messageValue);
+    }
+
+    @KafkaListener(topics = "test", groupId = "test-group-02", properties = {"max.poll.interval.ms=60000", "auto.offset.reset:earliest"})
+    public void topicWithPropertiesListener(String messageValue) {
+        logger.info(messageValue);
+    }
+
+    @KafkaListener(topics = "test", groupId = "test-group-03", concurrency = "3")
+    public void concurrentTopicListener(String messageValue) {
+        logger.info(messageValue);
+    }
+
+    @KafkaListener(topicPartitions = {
+            @TopicPartition(topic = "test01", partitions = {"0", "1"}),
+            @TopicPartition(topic = "test02", partitionOffsets = @PartitionOffset(partition = "0", initialOffset = "3"))
+    },
+    groupId = "test-group-04")
+    public void listenSpecificPartition(ConsumerRecord<String, String> record) {
+        logger.info(record.toString());
     }
 
 }
