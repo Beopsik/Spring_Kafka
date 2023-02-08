4f13dee7257f8866fd011f9a2a7ca416f6a2cff6
Beopsik <mbsik6082@naver.com>
2023-02-08 17:04:55 +0900

    Init spring-kafka-batch-listener

---
+++ spring-kafka-batch-listener/src/main/java/com/example/springkafkabatchlistener/SpringKafkaBatchListenerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkafkabatchlistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaBatchListenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaBatchListenerApplication.class, args);
+    }
+
+}
---
+++ spring-kafka-batch-listener/src/test/java/com/example/springkafkabatchlistener/SpringKafkaBatchListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkafkabatchlistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaBatchListenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
