15a45873915a02438b4533fc67661796ab23c4e9
Beopsik <mbsik6082@naver.com>
2023-02-08 16:32:38 +0900

    Add 커스텀 KafkaTempalte

--- spring-kafka-template-producer/src/main/java/com/example/springkafkatemplateproducer/SpringKafkaTemplateProducerApplication.java
+++ spring-kafka-template-producer/src/main/java/com/example/springkafkatemplateproducer/SpringKafkaTemplateProducerApplication.java
@@ -1,13 +1,38 @@
 package com.example.springkafkatemplateproducer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaProducerException;
+import org.springframework.kafka.core.KafkaSendCallback;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 
 @SpringBootApplication
-public class SpringKafkaTemplateProducerApplication {
+public class SpringKafkaTemplateProducerApplication implements CommandLineRunner {
+    private static final String TOPIC_NAME = "test";
+
+    @Autowired
+    private KafkaTemplate<String, String> customKafkaTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringKafkaTemplateProducerApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        ListenableFuture<SendResult<String, String>> future = customKafkaTemplate.send(TOPIC_NAME, "test"); // 전송한 이후에 정상 적재되었는지 여부를 확인
+        future.addCallback(new KafkaSendCallback<String, String>() { // 비동기로 적재 여부 확인
+            @Override
+            public void onSuccess(SendResult<String, String> result) { // 정상 적재되면 호출됨
+            }
+
+            @Override
+            public void onFailure(KafkaProducerException ex) { // 적재되지 않고 이슈 발생하면 호출됨
+            }
+        });
+        System.exit(0);
+    }
 }
