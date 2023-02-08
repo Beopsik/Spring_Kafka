1293882e887f8f5566ca88e0494fe034e511ead2
Beopsik <mbsik6082@naver.com>
2023-02-08 16:18:34 +0900

    Add 기본 KafkaTemplate

--- spring-kafka-producer/src/main/java/com/example/springkafkaproducer/SpringKafkaProducerApplication.java
+++ spring-kafka-producer/src/main/java/com/example/springkafkaproducer/SpringKafkaProducerApplication.java
@@ -1,13 +1,28 @@
 package com.example.springkafkaproducer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
-public class SpringKafkaProducerApplication {
+public class SpringKafkaProducerApplication implements CommandLineRunner {
+
+    private static final String TOPIC_NAME = "test";
+
+    @Autowired
+    private KafkaTemplate<Integer, String> template;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringKafkaProducerApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        for (int i = 0; i < 10; i++) {
+            template.send(TOPIC_NAME, "test" + i);
+        }
+        System.exit(0);
+    }
 }
