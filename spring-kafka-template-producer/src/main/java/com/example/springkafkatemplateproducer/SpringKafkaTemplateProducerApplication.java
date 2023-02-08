a840d58b4da73d6690c3bd56017709dd5b1e8341
Beopsik <mbsik6082@naver.com>
2023-02-08 16:20:43 +0900

    Init spring-kafka-template-producer

---
+++ spring-kafka-template-producer/src/main/java/com/example/springkafkatemplateproducer/SpringKafkaTemplateProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkafkatemplateproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaTemplateProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaTemplateProducerApplication.class, args);
+    }
+
+}
