fb0c72bd6c600fdfe6d95c56df21a600b2831ead
Beopsik <mbsik6082@naver.com>
2023-02-08 17:20:05 +0900

    Init spring-kafka-listener-container

---
+++ spring-kafka-listener-container/src/main/java/com/example/springkafkalistenercontainer/SpringKafkaListenerContainerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkafkalistenercontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaListenerContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaListenerContainerApplication.class, args);
+	}
+
+}
