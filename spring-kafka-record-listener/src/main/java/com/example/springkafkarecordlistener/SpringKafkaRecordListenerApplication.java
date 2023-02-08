ced3d49f2204291f0652396ea2773687996cd6c5
Beopsik <mbsik6082@naver.com>
2023-02-08 16:51:13 +0900

    Init spring-kafka-record-listener

---
+++ spring-kafka-record-listener/src/main/java/com/example/springkafkarecordlistener/SpringKafkaRecordListenerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkafkarecordlistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaRecordListenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaRecordListenerApplication.class, args);
+    }
+
+}
---
+++ spring-kafka-record-listener/src/test/java/com/example/springkafkarecordlistener/SpringKafkaRecordListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkafkarecordlistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaRecordListenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
