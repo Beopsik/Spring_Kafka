7477cfe804adda3ee993d5fe2dd5455c3f288491
Beopsik <mbsik6082@naver.com>
2023-02-08 17:31:49 +0900

    Add spring-kafka-listener-container

--- spring-kafka-listener-container/src/main/java/com/example/springkafkalistenercontainer/SpringKafkaListenerContainerApplication.java
+++ spring-kafka-listener-container/src/main/java/com/example/springkafkalistenercontainer/SpringKafkaListenerContainerApplication.java
@@ -1,13 +1,23 @@
 package com.example.springkafkalistenercontainer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 public class SpringKafkaListenerContainerApplication {
 
+	public static Logger logger = LoggerFactory.getLogger(SpringKafkaListenerContainerApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringKafkaListenerContainerApplication.class, args);
+
 	}
 
+	@KafkaListener(topics = "test", groupId = "test-group", containerFactory = "customContainerFactory")
+	public void customListener(String data) {
+		logger.info(data);
+	}
 }
