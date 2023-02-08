2dd3e9c0cfa34ddbe00e4e252a71d26d525571b7
Beopsik <mbsik6082@naver.com>
2023-02-08 17:10:34 +0900

    Add spring-kafka-batch-listener

--- spring-kafka-batch-listener/src/main/java/com/example/springkafkabatchlistener/SpringKafkaBatchListenerApplication.java
+++ spring-kafka-batch-listener/src/main/java/com/example/springkafkabatchlistener/SpringKafkaBatchListenerApplication.java
@@ -1,13 +1,34 @@
 package com.example.springkafkabatchlistener;
 
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringKafkaBatchListenerApplication {
+    public static Logger logger = LoggerFactory.getLogger(SpringKafkaBatchListenerApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringKafkaBatchListenerApplication.class, args);
     }
 
+    @KafkaListener(topics = "test", groupId = "test-group-01")
+    public void batchListener(ConsumerRecords<String, String> records) {
+        records.forEach(record -> logger.info(record.toString()));
+    }
+
+    @KafkaListener(topics = "test", groupId = "test-group-02")
+    public void batchListener(List<String> list) {
+        list.forEach(recordValue -> logger.info(recordValue));
+    }
+
+    @KafkaListener(topics = "test", groupId = "test-group-03", concurrency = "3")
+    public void concurrentBatchListener(ConsumerRecords<String, String> records) {
+        records.forEach(record -> logger.info(record.toString()));
+    }
 }
